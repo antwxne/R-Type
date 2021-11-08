78275251ccd641486d6d642911099f42eb53b9e5
Tom Rives <tom.rives@epitech.eu>
2021-11-08 19:07:52 +0100

    [ADD] Components fight

---
+++ src/Component/Speed.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2021
+** B-CPP-501-PAR-5-1-rtype-thomas1.tricaud
+** File description:
+** Speed
+*/
+
+#ifndef SPEED_HPP_
+#define SPEED_HPP_
+
+struct Speed
+{
+    unsigned int speed;
+};
+
+
+#endif /* !SPEED_HPP_ */
---
+++ src/Component/Tag.hpp
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2021
+** B-CPP-501-PAR-5-1-rtype-thomas1.tricaud
+** File description:
+** Tag
+*/
+
+#ifndef TAG_HPP_
+#define TAG_HPP_
+
+#define PLAYER 0
+#define ENEMY 1
+#define FRIEND 2
+#define POWERUP 3
+
+struct Tag
+{
+    int tag;
+};
+
+
+#endif /* !TAG_HPP_ */
