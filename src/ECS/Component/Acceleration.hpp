96b88a8725dea1924bb3d805af4f9ff1383e5a81
Alan Sigal <alan.sigal@epitech.eu>
2021-11-12 00:32:40 +0100

    [FIX] change component directory

---
+++ src/ECS/Component/Acceleration.hpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2021
+** B-CPP-501-PAR-5-1-rtype-thomas1.tricaud
+** File description:
+** Acceleration
+*/
+
+#ifndef ACCELERATION_HPP_
+#define ACCELERATION_HPP_
+
+/*
+x: 1 RIGHT x: -1 LEFT 0 NONE
+y: 1 UP y: -1 DOWN 0 NONE
+    acceleration *= speed
+    postion += acceleration
+*/
+
+struct Acceleration
+{
+    int x;
+    int y;
+};
+
+#endif /* !ACCELERATION_HPP_ */
---
+++ src/ECS/Component/Firerate.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2021
+** B-CPP-501-PAR-5-1-rtype-thomas1.tricaud
+** File description:
+** Firerate
+*/
+
+#ifndef FIRERATE_HPP_
+#define FIRERATE_HPP_
+
+struct Firerate
+{
+    unsigned int delay;
+};
+
+
+#endif /* !FIRERATE_HPP_ */
