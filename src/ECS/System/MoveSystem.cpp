7840d1c4e0dffbc0857b5561d2843ff4e77c4173
Tom Rives <tom.rives@epitech.eu>
2021-11-12 16:33:42 +0100

    [ADD] MoveSystem (in progress)

---
+++ src/ECS/System/MoveSystem.cpp
@@ -0,0 +1,36 @@
+/*
+** EPITECH PROJECT, 2021
+** B-CPP-501-PAR-5-1-rtype-thomas1.tricaud
+** File description:
+** MoveSystem
+*/
+
+#include "MoveSystem.hpp"
+#include "../Component/Speed.hpp"
+#include "../Component/Acceleration.hpp"
+#include "../Component/Transform/Position.hpp"
+
+MoveSystem::MoveSystem(std::shared_ptr<ComponentManager::ComponentsMap_t> &components) : ASystem(components)
+{
+    _usedComponents.push_back(typeid(Speed));
+    _usedComponents.push_back(typeid(Acceleration));
+    _usedComponents.push_back(typeid(Position));
+}
+
+MoveSystem::~MoveSystem()
+{
+}
+
+void MoveSystem::update()
+{
+    for (std::size_t i = 0; i < MAX_ENTITIES; i++) {
+        if (!checkAvaiableEntity(i))
+            continue;
+        auto &position = std::any_cast<Position&>(_components->at(typeid(Position))->getData(i).value());
+        auto &speed = std::any_cast<Speed&>(_components->at(typeid(Speed))->getData(i).value());
+        auto &acceleration = std::any_cast<Acceleration&>(_components->at(typeid(Acceleration))->getData(i).value());
+
+        position.x += acceleration.x * speed.speed;
+        position.y += acceleration.y * speed.speed;
+    }
+}
---
+++ src/ECS/System/MoveSystem.hpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2021
+** B-CPP-501-PAR-5-1-rtype-thomas1.tricaud
+** File description:
+** MoveSystem
+*/
+
+#ifndef MOVESYSTEM_HPP_
+#define MOVESYSTEM_HPP_
+
+#include "ASystem.hpp"
+#include <vector>
+
+class MoveSystem : public ASystem {
+    public:
+        MoveSystem(std::shared_ptr<ComponentManager::ComponentsMap_t> &components);
+        ~MoveSystem();
+        void update() override;
+
+    protected:
+    private:
+};
+
+#endif /* !MOVESYSTEM_HPP_ */
