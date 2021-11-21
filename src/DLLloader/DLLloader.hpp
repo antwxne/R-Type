59b4f4bc3c3c7d3388c208ef1eab50d2919fba28
Alan Sigal <alan.sigal@epitech.eu>
2021-11-21 23:39:52 +0100

    [ADD] bonus

--- src/DLLloader/DLLloader.hpp
+++ src/DLLloader/DLLloader.hpp
@@ -39,12 +39,7 @@ DLLloader<T>::DLLloader(const std::string &filename, std::string &symbol)
 template<typename T>
 DLLloader<T>::~DLLloader()
 {
-    std::cout << "DlClose" << std::endl;
     _instance.reset();
-    // if (_handle != NULL) {
-    //     std::cout << "On Cdlclose !" << std::endl;
-    //     Cdlclose();
-    // }
 }
 
 template<typename T>
